90ec78327df444589bc9b9940f4145cac06918a2
Nikoloz Naskidashvili <nikoloz2005@gmail.com>
2024-04-29 10:49:35 +0400

    feat: add my work

--- midterm/nikoloz_naskidashvili_1/task1/Fantasy.java
+++ midterm/nikoloz_naskidashvili_1/task1/Fantasy.java
@@ -1,13 +1,32 @@
 package midterm.nikoloz_naskidashvili_1.task1;
 
+
 public class Fantasy extends AbstractFantasy implements Dream {
-    // TODO change studentName to your name
-    private String studentName;
+    private final String studentName = "Nikoloz Naskidashvili";
+
+    public String getSuitcase1() {
+        return studentName + "'s suitcase";
+    }
+
+    public String getDigital2() {
+        return studentName + "'s digital";
+    }
+
+    public String methodAlgorithm3(String argCaucasus5) {
+        return studentName + "'s algorithm";
+    }
+
+    public java.util.List<String> methodBird4(String argSandpaper6) {
+        return java.util.Collections.emptyList();
+    }
 
     public String toString() {
         String result = "";
 
-        // TODO must be implemented
+        result += getSuitcase1() + "\n";
+        result += getDigital2() + "\n";
+        result += methodAlgorithm3("argCaucasus5") + "\n";
+        result += methodBird4("argSandpaper6") + "\n";
         return result;
     }
 }
